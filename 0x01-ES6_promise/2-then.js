778145bc94289a1b9ca397ebf51e15d9a064cf5f
Mo2men-dev <momensameh5@gmail.com>
2024-07-23 19:32:37 +0300

    task_2: done

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Promise} promise
+ *
+ * @return {Promise}
+ */
+export default function getResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(
+      () => new Error('The fake API is not working currently'),
+    ).finally(
+      () => console.log('Got a response from the API'),
+    );
+}
