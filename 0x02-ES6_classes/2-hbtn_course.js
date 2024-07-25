faeb85b4a85275e55305d29fb8a10d065bcc8dab
Mo2men-dev <momensameh5@gmail.com>
2024-07-25 23:40:47 +0300

    task_2: linting

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,67 +1,43 @@
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  /**
-     * Setter for the name attribute
-     * @param {String} name - The name of the course
-     */
-  set name(newName) {
-    if (typeof newName !== 'string') {
+  set name(name) {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
-
-    this._name = newName;
+    this._name = name;
   }
 
-  /**
-     * Getter for the name attribute
-     * @return {String} - The name of the course
-    */
   get name() {
     return this._name;
   }
 
-  /**
-     * Setter for the length attribute
-     * @param {Number} length - The length of the course
-    */
-  set length(newLength) {
-    if (typeof newLength !== 'number') {
+  set length(length) {
+    if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
-
-    this._length = newLength;
+    this._length = length;
   }
 
-  /**
-     * Getter for the length attribute
-     * @return {Number} - The length of the course
-    */
   get length() {
     return this._length;
   }
 
-  /**
-     * Setter for the students attribute
-     * @param {Array} students - The students in the course
-    */
-  set students(newStudents) {
-    if (!Array.isArray(newStudents)) {
-      throw new TypeError('Students must be an array');
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an Array');
     }
-
-    this._students = newStudents;
   }
 
-  /**
-     * Getter for the students attribute
-     * @return {Array} - The students in the course
-    */
   get students() {
     return this._students;
   }
 }
+
+export default HolbertonCourse;
