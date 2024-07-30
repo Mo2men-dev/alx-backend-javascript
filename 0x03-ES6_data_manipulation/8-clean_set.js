c4ad98df1f223a9bbe63c9b0150662497a66d853
Mo2men-dev <momensameh5@gmail.com>
2024-07-30 17:11:35 +0300

    task_8: update to function

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,13 @@
-export default function cleanSet(set, startString) {
-  const arr = [];
-  set.forEach((value) => {
-    if (typeof value === 'string' && value.startsWith(startString)) {
-      arr.push(value.slice(startString.length));
+const cleanSet = (set, startString) => {
+  const strings = [];
+
+  if (startString === '' || typeof startString !== 'string') return '';
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      strings.push(s.slice(startString.length));
     }
   });
+  return strings.join('-');
+};
 
-  return arr.join('-');
-}
+export default cleanSet;
